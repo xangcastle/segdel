c5364b507a4dc9b4475bc25743c1d24e8cb1caf7
twine03 <twinewilliams03@gmail.com>
2016-11-06 16:48:26 -0600

    control de existencias multibodega

--- inventario/admin.py
+++ inventario/admin.py
@@ -4,3 +4,13 @@ from import_export.admin import ImportExportModelAdmin
 from inventario.models import *
 
 admin.site.register(Import_Imventario, ImportExportModelAdmin)
+
+class producto_admin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre')
+admin.site.register(Producto)
+
+class bodega_admin(admin.ModelAdmin):
+    list_display = ('nombre', 'encargado')
+admin.site.register(Bodega)
+
+admin.site.register(Bodega_Detalle)
--- inventario/models.py
+++ inventario/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 from app.models import Empresa
@@ -9,11 +10,15 @@ class Import_Imventario(models.Model):
     razon_social = models.CharField(max_length=255)
     numero_ruc = models.CharField(max_length=14)
     producto_codigo = models.CharField(max_length=10)
+    producto_serie = models.CharField(max_length=50)
     producto_nombre = models.CharField(max_length=200)
-    producto_existencia = models.FloatField()
+    producto_existencia = models.FloatField(null=False, default=0)
+    producto_costo = models.FloatField(null=False, default=0)
+    producto_precio = models.FloatField(null=False, default=0)
     producto_marca = models.CharField(max_length=100)
     producto_categoria = models.CharField(max_length=100)
     producto_medida = models.CharField(max_length=100)
+    bodega = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = "opcion"
@@ -56,22 +61,46 @@ class Import_Imventario(models.Model):
                 categoria=self.producto_categoria)
         return o
 
-    def get_producto(self):
+    def get_bodega(self):
         o = None
         try:
-            o = Producto.objects.get(codigo=self.producto_codigo)
+            o = Bodega.objects.get(nombre=self.bodega)
         except:
-            o, create = Producto.objects.get_or_create(
+            o, create = Bodega.objects.get_or_create(
+                nombre=self.bodega)
+        return o
+
+    def get_producto(self):
+        producto = None
+        try:
+            producto = Producto.objects.get(codigo=self.producto_codigo, serie=self.producto_serie)
+        except:
+            producto, create = Producto.objects.get_or_create(
                 codigo=self.producto_codigo,
+                serie=self.producto_serie,
                 nombre=self.producto_nombre,
                 categoria=self.get_categoria(),
                 empresa=self.get_empresa(),
                 medida=self.get_medida(),
-                marca=self.get_marca(),)
-        return o
+                marca=self.get_marca(),
+                costo_promedio=self.producto_costo,
+                precio=self.producto_precio)
+
+        # REGISTRO DE LA EXISTENCIA
+        bodega = self.get_bodega()
+        detalle = None
+        try:
+            detalle = Bodega_Detalle.objects.get(bodega=bodega, producto=producto)
+            detalle.existencia += self.producto_existencia
+        except:
+            detalle, create = Bodega_Detalle.objects.get_or_create(bodega=bodega, producto=producto,
+                                                                   existencia=self.producto_existencia)
+
+        return producto
 
     def save(self, *args, **kwargs):
         self.get_producto()
+
 
 class Producto_Marca(models.Model):
     marca = models.CharField(max_length=100)
@@ -79,11 +108,13 @@ class Producto_Marca(models.Model):
     def __unicode__(self):
         return "%s" % (self.marca)
 
+
 class Producto_Categoria(models.Model):
     categoria = models.CharField(max_length=100)
 
     def __unicode__(self):
         return "%s" % (self.categoria)
+
 
 class Producto_Medida(models.Model):
     medida = models.CharField(max_length=100)
@@ -91,13 +122,42 @@ class Producto_Medida(models.Model):
     def __unicode__(self):
         return "%s" % (self.medida)
 
+
 class Producto(models.Model):
     codigo = models.CharField(max_length=10)
+    serie = models.CharField(max_length=50)  # REGISTRO DE PRODUCTOS POR SERIE
     nombre = models.CharField(max_length=200)
+    costo_promedio = models.FloatField(default=0, null=False)
+    precio = models.FloatField(default=0, null=False)
     categoria = models.ForeignKey(Producto_Categoria)
     medida = models.ForeignKey(Producto_Medida)
     marca = models.ForeignKey(Producto_Marca)
     empresa = models.ForeignKey(Empresa)
 
+    class Meta:
+        verbose_name = "opcion"
+        verbose_name_plural = "Productos del Invetnario"
+
     def __unicode__(self):
-        return "%s" % (self.medida)
+        return "%s %s" % (self.codigo, self.nombre)
+
+
+class Bodega(models.Model):
+    nombre = models.CharField(max_length=100, null=False)
+    encargado = models.ForeignKey(User, null=True)
+
+    def __unicode__(self):
+        return "%s" % (self.nombre)
+
+
+class Bodega_Detalle(models.Model):
+    bodega = models.ForeignKey(Bodega, null=False)
+    producto = models.ForeignKey(Producto, null=False)
+    existencia = models.FloatField(default=0.0, null=False)
+
+    class Meta:
+        verbose_name = "opcion"
+        verbose_name_plural = "Detalle de bodegas"
+
+    def __unicode__(self):
+        return "%s %s-%s" % (self.bodega.nombre, self.producto, self.existencia)
